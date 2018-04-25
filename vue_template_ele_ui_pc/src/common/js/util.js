82a60789a90704bcd2a4dfa0e6e44e80e6cc0864
bannerl <=>
2018-04-25 19:16:40 +0800

    add element-ui pc

---
+++ vue_template_ele_ui_pc/src/common/js/util.js
@@ -0,0 +1,21 @@
+/**
+ * 解析url参数
+ * @example '?id=1234&collect=true'
+ * @return object id:1234,collect:true
+ */
+export function loadUrl(url) {
+	let obj = {};
+	let reg = /[?&][^?&]+=[^?&]+/g;
+	let arr = url.match(reg);
+	let i = 0;
+	if(arr){
+		for(;i<arr.length;i++){
+			let str = arr[i].substring(1);
+			let tempArr = str.split('=');
+			let key = tempArr[0];
+			let value = tempArr[1];
+			obj[key] = value
+		}
+	}	
+	return obj;
+}
---
+++ vue_template_ele_ui_pc/src/page/order/order.vue
@@ -0,0 +1,39 @@
+<style lang="scss" type="text/css">
+	.index-wrapper{
+		
+	}
+	/*.index-enter-active,.index-leave-active{
+		transition: all .2s;
+		transform: translate3d(0,0,0);
+	}
+	.index-enter,.index-leave-to{
+		transform: translate3d(-100%,0,0);
+	}*/
+</style> 
+<template>
+	<div>
+		<header>头部</header>
+		<slot name="fixed-navbar"></slot>
+	</div>		
+</template>
+
+</script>
+<script>
+	// import { Indicator,Toast } from 'mint-ui'
+	// import BScroll from 'better-scroll'
+	// import {mapState,mapMutations} from 'vuex'
+	// import {setStore, getStore} from '@/common/js/savaLocal'
+	// import {setDocumentTitle} from '@/common/js/base'
+	
+	export default {
+      data: function() {
+        return {
+        	
+        }
+      },
+      computed:{
+      	
+      }
+    
+    }
+</script>
